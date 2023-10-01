5bbc7134b75912f5b64ff571410ccd036b1e3d0a
Michelle <michelle-jolink@hotmail.com>
2023-10-01 19:09:53 +0200

    setup hibernate.cfg.xml, connecting to db and inserting

---
+++ spotitube-backend-JPA/src/main/java/nl/han/oose/dea/spotitube/APp.java
@@ -0,0 +1,10 @@
+package nl.han.oose.dea.spotitube;
+
+public class APp {
+
+  public static TestApp testApp = new TestApp();
+  public static void main(String[] args) {
+    testApp.setUp();
+    testApp.test();
+  }
+}
---
+++ spotitube-backend-JPA/src/main/java/nl/han/oose/dea/spotitube/TestApp.java
@@ -0,0 +1,45 @@
+package nl.han.oose.dea.spotitube;
+
+import nl.han.oose.dea.spotitube.business.dto.JPA.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class TestApp {
+
+  private SessionFactory sessionFactory;
+
+  protected void setUp() {
+    // A SessionFactory is set up once for an application!
+    final StandardServiceRegistry registry =
+      new StandardServiceRegistryBuilder()
+        .configure() // Hiermee wordt de standaard Hibernate-configuratie gelezen
+        .build();
+    try {
+      MetadataSources metadataSources =
+        new MetadataSources(registry)
+          .addAnnotatedClass(User.class);
+      Metadata metadata = metadataSources.buildMetadata();
+      sessionFactory = metadata.buildSessionFactory();
+    } catch (Exception e) {
+      // The registry would be destroyed by the SessionFactory, but we
+      // had trouble building the SessionFactory so destroy it manually.
+      StandardServiceRegistryBuilder.destroy(registry);
+      e.printStackTrace();
+    }
+  }
+
+
+  protected void test(){
+    sessionFactory.inTransaction(session -> {
+      session.persist(new User("bla", "fdsfdsfdsf", "hii"));
+    });
+  }
+
+
+
+
+
+}
