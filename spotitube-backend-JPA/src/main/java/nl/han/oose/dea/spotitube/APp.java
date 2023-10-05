cd153921848a1115849ee82be86d038e80ca68c3
Michelle <michelle-jolink@hotmail.com>
2023-10-05 16:22:31 +0200

    query with HQL

--- spotitube-backend-JPA/src/main/java/nl/han/oose/dea/spotitube/APp.java
+++ spotitube-backend-JPA/src/main/java/nl/han/oose/dea/spotitube/APp.java
@@ -3,6 +3,8 @@ package nl.han.oose.dea.spotitube;
 import nl.han.oose.dea.spotitube.business.dto.JPA.User;
 import org.hibernate.Session;
 
+import java.util.List;
+
 public class APp {
 
   public static SetupSessionFactory sessionFactoryManager = new SetupSessionFactory();
@@ -13,8 +15,13 @@ public class APp {
     // Setup de verbinding met db
     sessionFactoryManager.setUp();
 
+//    save_user_to_db();
+      fetch_users_with_hql();
+  }
+
+  public static void save_user_to_db(){
     // Maak object aan die je wil opslaan
-    User testUser = new User("miechelle", "fdsfdsfds", "password9324324");
+    User testUser = new User("miessschelle", "fdsfdsfds", "password9324324");
 
     // Start de verbinding
     try (Session session = sessionFactoryManager.getSessionFactory().openSession()) {
@@ -23,6 +30,22 @@ public class APp {
 
       // Sla object op in db
       session.persist(testUser);
+
+      // Beëindig transactie
+      session.getTransaction().commit();
+    }
+  }
+
+  public static void fetch_users_with_hql(){
+    // Start de verbinding
+    try (Session session = sessionFactoryManager.getSessionFactory().openSession()) {
+      // begin transactie
+      session.beginTransaction();
+
+      // Vraag alle users op die bestaan in de db van je User object
+      List<User> users = session.createQuery("select u from User u", User.class).stream().toList();
+
+      users.forEach(user -> System.out.println("Usernames: " + user.getUser()));
 
       // Beëindig transactie
       session.getTransaction().commit();
