4795e1f7d595371374dbfe71700bb38b6cef4925
Michelle <michelle-jolink@hotmail.com>
2023-10-05 16:15:34 +0200

    Basic CRUD operation with HIbernate

--- spotitube-backend-JPA/src/main/java/nl/han/oose/dea/spotitube/APp.java
+++ spotitube-backend-JPA/src/main/java/nl/han/oose/dea/spotitube/APp.java
@@ -1,10 +1,31 @@
 package nl.han.oose.dea.spotitube;
 
+import nl.han.oose.dea.spotitube.business.dto.JPA.User;
+import org.hibernate.Session;
+
 public class APp {
 
-  public static TestApp testApp = new TestApp();
+  public static SetupSessionFactory sessionFactoryManager = new SetupSessionFactory();
+
+  // De session houdt in feite de verbinding met de db.
+
   public static void main(String[] args) {
-    testApp.setUp();
-    testApp.getData();
+    // Setup de verbinding met db
+    sessionFactoryManager.setUp();
+
+    // Maak object aan die je wil opslaan
+    User testUser = new User("miechelle", "fdsfdsfds", "password9324324");
+
+    // Start de verbinding
+    try (Session session = sessionFactoryManager.getSessionFactory().openSession()) {
+      // begin transactie
+      session.beginTransaction();
+
+      // Sla object op in db
+      session.persist(testUser);
+
+      // Beëindig transactie
+      session.getTransaction().commit();
+    }
   }
 }
--- spotitube-backend-JPA/src/main/java/nl/han/oose/dea/spotitube/SetupSessionFactory.java
+++ spotitube-backend-JPA/src/main/java/nl/han/oose/dea/spotitube/SetupSessionFactory.java
@@ -7,9 +7,7 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import static java.lang.System.out;
-
-public class TestApp {
+public class SetupSessionFactory {
 
   private SessionFactory sessionFactory;
 
@@ -33,20 +31,23 @@ public class TestApp {
     }
   }
 
-
-  protected void test(){
-    sessionFactory.inTransaction(session -> {
-      session.persist(new User("bla", "fdsfdsfdsf", "hii"));
-    });
+  public SessionFactory getSessionFactory() {
+    return sessionFactory;
   }
 
-  protected void getData(){
-    sessionFactory.inTransaction(session -> {
-      session.createSelectionQuery("from User", User.class)
-        .getResultList()
-        .forEach(user -> out.println("User (" + user.getUser() + ") : " + user.getToken()));
-    });
-  }
+  //  protected void test(){
+//    sessionFactory.inTransaction(session -> {
+//      session.persist(new User("bla", "fdsfdsfdsf", "hii"));
+//    });
+//  }
+//
+//  protected void getData(){
+//    sessionFactory.inTransaction(session -> {
+//      session.createSelectionQuery("from User", User.class)
+//        .getResultList()
+//        .forEach(user -> out.println("User (" + user.getUser() + ") : " + user.getToken()));
+//    });
+//  }
 
 
 
